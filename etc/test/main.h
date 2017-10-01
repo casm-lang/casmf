048637e2ee5ff70457c536063dcceec3e4f6b81f
Philipp Paulweber <p.paulweber@gmail.com>
2017-10-02 00:19:07 +0200

    Package
    * updated LibPackage CMake module
    * updated CMake build script
    * added test and benchmark suite
    * fixed main source file

---
+++ etc/test/main.h
@@ -0,0 +1,40 @@
+//
+//  Copyright (c) 2014-2017 CASM Organization
+//  All rights reserved.
+//
+//  Developed by: Philipp Paulweber
+//                Emmanuel Pescosta
+//                https://github.com/casm-lang/casmf
+//
+//  This file is part of casmf.
+//
+//  casmf is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  casmf is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with casmf. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef _CASMF_TEST_MAIN_H_
+#define _CASMF_TEST_MAIN_H_
+
+#include <libstdhl/Test>
+
+#endif // _CASMF_TEST_MAIN_H_
+
+//
+//  Local variables:
+//  mode: c++
+//  indent-tabs-mode: nil
+//  c-basic-offset: 4
+//  tab-width: 4
+//  End:
+//  vim:noexpandtab:sw=4:ts=4:
+//
